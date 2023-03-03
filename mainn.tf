4ef9fb3be92d66bb2131d04587306c42f1c0ee02
monica <monicamatta1997@gmil.com>
2023-03-03 14:46:08 +0200

    eip

--- mainn.tf
+++ mainn.tf
@@ -3,7 +3,17 @@ resource "aws_instance" "my-ec2"{
     instance_type="t2.micro"
     tags = {
          Name = "terrainstance" 
-    } 
+    }
 }
+resource  "aws_eip" "my-eip"{
+    vpc = true
+}
+
+resource "aws_eip_association" "associate"{
+    instance_id=aws_instance.my-ec2.id
+    allocation_id=aws_eip.my-eip.id
+
+}
+
 
     
